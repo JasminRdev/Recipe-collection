aacfc03551517d69c674a6e650e5be05d8c91988
Jasmin <eyesthestic@gmail.com>
2024-03-09 12:57:35 +0100

    fundamental style and structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Recipe-collection
 Here are my Recipes collected. You can filter through and add new ones. But currently it is based on my local db. Just an website to practice my coding skills in general and share on of my favourite hobby.
+
+
+-navigate to backend folder
+-execute npm start to start the server
+-go to http://localhost:3000/
--- frontend/ejs/public/script.js
+++ frontend/ejs/public/script.js
@@ -1,15 +1,108 @@
-fetch("/api/recipes")
-.then(res => res.json())
-.then(recipes => {
-    const recipeContainer = document.getElementById('apiData')
-    recipes.forEach(item =>  {
-        const listItem = document.createElement('p');
-        listItem.textContent = `${item.title}`
+function buildRecipeUp(recipeResult, recipeContainer){
+    recipeResult.forEach(item =>  {
+        const titleItem = document.createElement('h2');
+        const quickInfo = document.createElement('p');
+        const description = document.createElement('h5');
+
+        const listItem = document.createElement('article');
+        const linkEl = document.createElement('a');
+        let titlesRecipe = item.title;
+        let titleWithoutSpaces = `${titlesRecipe.replace(/\s+/g,'-')}`
+        linkEl.href = titleWithoutSpaces
+        linkEl.setAttribute("id",titleWithoutSpaces )
+
+        const mainContentRecipe = document.createElement('section');
+        const image = document.createElement('img');
+        const rating = document.createElement('aside')
+
+        function stars(number) {
+            for(let star = 0; star < number; star++) {
+                const ratingTag = document.createElement('i')
+                ratingTag.classList.add("fas")
+                ratingTag.classList.add("fa-star")
+                
+                rating.appendChild(ratingTag)
+            }
+        }
+        stars(item.stars)
+
+        image.src = "cooking.jpg";
+        image.alt = "cooking pan";
+        image.classList.add("recipeImage")
+        
+        listItem.classList.add("containerApi")
+        linkEl.classList.add("itemApi")
+
+        let meal = item.mealTimes;
+        let singularOrMultipe = item.oftenness === 1 ? "time" : "times"
+
+        titleItem.textContent = `${item.title}`
+        quickInfo.textContent = `${meal.join(", ")} - ${item.timeConsuming} - ${item.nutritionScore} - ${item.oftenness} ${singularOrMultipe} made`
+        description.textContent = `${item.description}`
+        
+
+        mainContentRecipe.appendChild(titleItem);
+        mainContentRecipe.appendChild(quickInfo);
+        mainContentRecipe.appendChild(description);
+        
+        listItem.appendChild(linkEl)
+        linkEl.appendChild(image);
+        linkEl.appendChild(mainContentRecipe);
+        linkEl.appendChild(rating)
         recipeContainer.appendChild(listItem);
-    })
+
+
 })
-.catch (error => {
-    console.error("Error fetching recipes: ", error);
+}
+
+function fetchAll() {
+    fetch("/api/recipes")
+    .then(res => res.json())
+    .then(recipes => {
+        const recipeContainer = document.getElementById('apiData')
+        buildRecipeUp(recipes, recipeContainer);
+    })
+    .catch (error => {
+        console.error("Error fetching recipes: ", error);
+    })
+}
+
+
+
+function cleanUp() {
+    const recipeContainer = document.getElementById('apiData');
+    const childrenEls = recipeContainer.children;
+    // loop starts in reverse to not skip any el
+    for (let i = childrenEls.length - 1; i >= 0; i--) {
+        const childEl = childrenEls[i];
+        recipeContainer.removeChild(childEl);
+        console.log(i, childEl);
+    }
+} 
+
+
+const searchForm = document.getElementById("searchRecipes")
+searchForm.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    let formData = new FormData(this);
+
+    for(let pair of formData.entries()) {
+        const choosenCategory = pair[1]
+        choosenCategory === "All categories" ? fetchAll() : fetch("/api/recipes")
+        .then(res => res.json())
+        .then(recipes => {
+            const recipeResult = recipes.filter((item) => item.mealTimes.includes(choosenCategory)
+        )
+        
+
+        const recipeContainer = document.getElementById('apiData')
+        cleanUp();
+        buildRecipeUp(recipeResult, recipeContainer );
+    })
+    }
 })
 
-console.log("new")
+
+
+fetchAll()
